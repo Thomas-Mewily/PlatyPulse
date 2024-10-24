4f57396694bd4735da0f1b6d8cf3b7cfcbe3ea44
thtamagnau <thomas.tamagnaud@etu.uca.fr>
2024-10-25 00:43:38 +0200

    icon change

--- PlatyPulseAPI/PlatyApp.cs
+++ PlatyPulseAPI/PlatyApp.cs
@@ -28,19 +28,19 @@ public class PlatyApp : PlatyAppComponent
         var run =
                 new Goal(GoalKind.Run,
                     [
-                    new Rank(100.Meter(), 10.XP()),
-                new Rank(200.Meter(), 30.XP()),
-                new Rank(500.Meter(), 70.XP()),
-                new Rank(1.KiloMeter(), 1500.XP()),
-                new Rank(3.KiloMeter(), 3500.XP()),
-                new Rank(5.KiloMeter(), 6500.XP()),
+                        new Rank(100.Meter(), 10.XP()),
+                        new Rank(200.Meter(), 30.XP()),
+                        new Rank(500.Meter(), 70.XP()),
+                        new Rank(1.KiloMeter(), 1500.XP()),
+                        new Rank(3.KiloMeter(), 3500.XP()),
+                        new Rank(5.KiloMeter(), 6500.XP()),
                     ]
                 );
 
         var push_up =
         new Goal(GoalKind.PushUp,
             [
-            new Rank(1.PushUp(), 1.XP()),
+                new Rank(1.PushUp(), 1.XP()),
                 new Rank(3.PushUp(), 5.XP()),
                 new Rank(10.PushUp(), 30.XP()),
                 new Rank(20.PushUp(), 100.XP()),
