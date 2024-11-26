b4d400f0e20345367021b6e11145e3f56f64a4dd
xerneas02 <mathis.roubille@gmail.com>
2024-11-26 17:19:55 +0100

    Tous controllers ajouter

--- PlatyPulseDB/Controllers/Controller.cs
+++ PlatyPulseDB/Controllers/Controller.cs
@@ -3,6 +3,7 @@ using PlatyPulseAPI.Data;
 
 namespace PlatyPulseWebAPI.Controllers;
 
+// Controller pour Quest
 [Route("api/[controller]")]
 [ApiController]
 public class QuestsController : GenericController<Quest>
@@ -10,9 +11,42 @@ public class QuestsController : GenericController<Quest>
     public QuestsController(DataBaseCtx dbContext) : base(dbContext) { }
 }
 
+// Controller pour Challenge
 [Route("api/[controller]")]
 [ApiController]
 public class ChallengesController : GenericController<Challenge>
 {
     public ChallengesController(DataBaseCtx dbContext) : base(dbContext) { }
+}
+
+// Controller pour User
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : GenericController<User>
+{
+    public UsersController(DataBaseCtx dbContext) : base(dbContext) { }
+}
+
+// Controller pour ChallengeEntry
+[Route("api/[controller]")]
+[ApiController]
+public class ChallengeEntriesController : GenericController<ChallengeEntry>
+{
+    public ChallengeEntriesController(DataBaseCtx dbContext) : base(dbContext) { }
+}
+
+// Controller pour QuestEntry
+[Route("api/[controller]")]
+[ApiController]
+public class QuestEntriesController : GenericController<QuestEntry>
+{
+    public QuestEntriesController(DataBaseCtx dbContext) : base(dbContext) { }
+}
+
+// Si Owned ou IdentifiableOwnedByData nécessitent des contrôleurs spécifiques
+[Route("api/[controller]")]
+[ApiController]
+public class OwnedsController : GenericController<Owned<IdentifiableOwnedByData>>
+{
+    public OwnedsController(DataBaseCtx dbContext) : base(dbContext) { }
 }
